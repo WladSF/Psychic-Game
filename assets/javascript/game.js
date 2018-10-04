534cc3580993ff97bd3f37fef963b5c2aa50e4c8
WladSF <wpaivasf@gmail.com>
2018-10-04 01:23:04 -0700

    Homework 3 update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,24 +7,40 @@
 //Create variables
 var wins = 0;
 var losses = 0;
-var guessesSoFar = 0;
+var guessesSoFar = [];
 var guessesLeft = 10;
 
 //When the user presses a key, it will run the following function://
-    document.onkeypress = function (event) {
+    document.onkeyup = function (event) {
 
-//Capture the key pressed by user, store in userGuess variable.
+//Determines which key was pressed.Capture the key pressed by user, store in userGuess variable.
     var userGuess = event.key;
 
-//Possible letters to choose from//
+//Create an array with possible letters to choose from//
     var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
-//Choosing a random letter out of the possible letters//
+//Choosing a random letter from the options array. This is the compiuter's guess. 
     var computerChoice = alphabet[Math.floor(Math.random () * alphabet.length)];
+
+//If user guesses correctly, wins go up 1 point//
+    if(userGuess === computerChoice){
+        wins++;
+    }
+    
+    else {
+        guessesLeft--;
+    }
+
+    if(guessesLeft = 0){
+        losses++
+    }
 
 //Console output to confirm if correct//
     console.log(computerChoice);
-}
+  
 
+document.getElementById('wins').innerHTML = "Wins: " + wins;
+document.getElementById('losses').innerHTML = "Losses: " + losses;
+document.getElementById('guessesLeft').innerHTML = "Guesses left: " + guesses;
 
-
+    }
