f685fbc0d02cc386aaa8afaa1b60a1bd4603bafc
WladSF <wpaivasf@gmail.com>
2018-12-28 23:10:39 -0800

    Fixed letters-charaters only to be accepted; created a reset button; cahnged css layout; added alert boxes.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,71 @@
-// Essentially, the app randomly picks a letter,//
-// and the user has to guess which letter the app chose //
-
-// Must create a variable for user input, computer choice, 
-//possible letters, wins, losses, guesses left, guesses so far.//
-
 //Creating global variables//
 var wins = 0;
 var losses = 0;
 var guessesSoFar = [];
 var guessesLeft = 10;
+var userGuess = "";
+
+//Create an array with possible letters to choose from//
+var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+//Choosing a random letter from the options array. This is the computer's choice//
+var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 
 //When the user presses a key, it will run the following function://
 document.onkeyup = function (event) {
 
-    //Determines which key was pressed.Capture the key pressed by user, stores it in userGuess variable.//
-    var userGuess = event.key.toUpperCase();
+    //Determines which key was pressed.Capture the key pressed by user, stores it in userGuess variable. Ensures is a character from the alphabet string.//
+    var userGuess = String.fromCharCode(event.keyCode).toUpperCase();
+    var check = alphabet.includes(userGuess);
 
-    //This will store and display the user's choice of letters. It will push it into guessesSoFar array.//
-    guessesSoFar.push(userGuess);
-    
-    //Create an array with possible letters to choose from.//
-    var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    if (check === false) {
+        alert("That was not a valid guess, try again!");
+        return false;
 
-    //Choosing a random letter from the options array. This is the computer's guess.//
-    var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-
-    //If user guesses correctly, wins go up 1 point, and the game is reset.//
-    if (userGuess === computerChoice) {
-        wins++;
-        gameReset();
-    }
-    //If user chooses wrong letter, guessesLeft should go down 1 point.// 
-    else {
+    } else if (check === true) {
         guessesLeft--;
-    }
+        guessesSoFar.push(userGuess);               //This will store and display the user's choice of letters. It will push it into guessesSoFar array.//
 
-    //If guessesLeft go down to zero, losses go up 1 point, and the game is reset.//
-    if (guessesLeft === 0) {
-        losses++;
-        gameReset();
-    }
+        if (userGuess === computerChoice) {         //If user guesses correctly, wins go up 1 point, and the game is reset.//
+            alert("Congrats, you won!");
+            wins++;
+            gameReset();
+        }
+
+        if (guessesLeft === 0) {                    //If guessesLeft go down to zero, losses go up 1 point, and the game is reset.//
+            alert("Game over, you lost!");
+            losses++;
+            boo.play();
+            gameReset();
+        }
+    };
+
     //Selecting//
-    document.getElementById('wins').innerHTML = "Wins: " + wins;
-    document.getElementById('losses').innerHTML = "Losses: " + losses;
-    document.getElementById('guessesLeft').innerHTML = "Guesses left: " + guessesLeft;
-    document.getElementById('guessesSoFar').innerHTML = "Your guesses so far: " + guessesSoFar;
-}
+    document.getElementById('guessesSoFar').textContent = "Your guesses so far: " + guessesSoFar;
+    document.getElementById('guessesLeft').textContent = "Guesses left: " + guessesLeft;
+    document.getElementById('wins').textContent = "Wins: " + wins;
+    document.getElementById('losses').textContent = "Losses: " + losses;
 
-//This is the function to tell the two variables below to reset.//
+    writeStats();
+};
+
+//This is the function to tell the three variables below to reset.//
 var gameReset = function () {
     guessesSoFar = [];
     guessesLeft = 10;
-}
+    computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)]
+};
 
-function writeStats () {
-    console.log(wins);
-    console.log(losses);
+function writeStats() {
     console.log(guessesSoFar);
     console.log(guessesLeft);
+    console.log(wins);
+    console.log(losses);
     console.log(computerChoice);
     console.log(userGuess);
-}
+};
 
-writeStats ();
+function hardReset() {
+    location.reload();
+};
+
